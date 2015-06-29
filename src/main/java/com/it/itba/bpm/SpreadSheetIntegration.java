8e8f4829c648d53ea8632755c6a7ce3aebe8f320
Alan Pomerantz <alan.pomerantz@almundo.com>
2015-06-29 17:43:41 -0300

    Added mocked Updated and Delete

--- src/main/java/com/it/itba/bpm/SpreadSheetIntegration.java
+++ src/main/java/com/it/itba/bpm/SpreadSheetIntegration.java
@@ -1,6 +1,5 @@
 package com.it.itba.bpm;
 
-// ...
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -29,12 +28,15 @@ import com.it.itba.bpm.model.form.ActiveSchoolsForm;
 import com.it.itba.bpm.model.form.ContactForm;
 import com.it.itba.bpm.model.form.Form;
 
-// ...
-
 public class SpreadSheetIntegration {
 
 	final static String GOOGLE_DRIVE_FEED_URL_STRING = "https://spreadsheets.google.com/feeds/spreadsheets/private/full";
 	private static SpreadsheetService service;
+
+	/*
+	 * 
+	 * https://developers.google.com/google-apps/spreadsheets/data
+	 */
 
 	static void init(Credential credential) throws IOException,
 			ServiceException, IllegalArgumentException, IllegalAccessException {
@@ -48,13 +50,196 @@ public class SpreadSheetIntegration {
 
 		populateSpreadsheetWithForm(service, contactForm, "ContactForm");
 		populateSpreadsheetWithForm(service, activeSchoolsForm,
-				"ActiveSchoolsForm");
+				"ActiveSchoolForm");
 
 		// Un metodo que te devuelva todos los colegios. Nombre de colegio -
 		// responsable. Devuelvo una collection del POJO.
 		// Hacer un CRUD para poder modificar rows. EL id es el mail.
 
 		// printDocumentsTitles();
+	}
+
+	private static boolean updateEntry(String email, String entry,
+			SpreadsheetService service, String spreadsheetName) {
+
+		// Define the URL to request. This should never change.
+		URL SPREADSHEET_FEED_URL = null;
+		try {
+			SPREADSHEET_FEED_URL = new URL(
+					"https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Make a request to the API and get all spreadsheets.
+		SpreadsheetFeed feed = null;
+		try {
+			feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+
+		if (spreadsheets.size() == 0) {
+			// TODO: There were no spreadsheets, act accordingly.
+		}
+
+		SpreadsheetEntry spreadsheet = null;
+
+		// Find spreadsheet by Name
+		for (SpreadsheetEntry spreadsheetEntry : spreadsheets) {
+			if (spreadsheetEntry.getTitle().getPlainText()
+					.equals(spreadsheetName))
+				spreadsheet = spreadsheetEntry;
+		}
+
+		if (spreadsheet == null)
+			throw new IllegalStateException("Spreadsheet Not Found");
+
+		System.out
+				.println("Going to Update entry with Id: " + email
+						+ " from Spreadsheet: "
+						+ spreadsheet.getTitle().getPlainText());
+
+		// Get the first worksheet of the first spreadsheet.
+		WorksheetFeed worksheetFeed = null;
+		try {
+			worksheetFeed = service.getFeed(spreadsheet.getWorksheetFeedUrl(),
+					WorksheetFeed.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
+		WorksheetEntry worksheet = worksheets.get(0);
+
+		// Fetch the list feed of the worksheet.
+		URL listFeedUrl = worksheet.getListFeedUrl();
+		ListFeed listFeed = null;
+		try {
+			listFeed = service.getFeed(listFeedUrl, ListFeed.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ListEntry row = listFeed.getEntries().get(0);
+
+		// Update the row's data.
+		row.getCustomElements().setValueLocal("firstname", "Sarah");
+		row.getCustomElements().setValueLocal("lastname", "Hunt");
+		row.getCustomElements().setValueLocal("age", "32");
+		row.getCustomElements().setValueLocal("height", "154");
+
+		// Save the row using the API.
+		try {
+			row.update();
+		} catch (IOException e) {
+			return false;
+		} catch (ServiceException e) {
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean deleteAllEntries(String email,
+			SpreadsheetService service, String spreadsheetName) {
+
+		// Define the URL to request. This should never change.
+		URL SPREADSHEET_FEED_URL = null;
+		try {
+			SPREADSHEET_FEED_URL = new URL(
+					"https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Make a request to the API and get all spreadsheets.
+		SpreadsheetFeed feed = null;
+		try {
+			feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+
+		if (spreadsheets.size() == 0) {
+			// TODO: There were no spreadsheets, act accordingly.
+		}
+
+		SpreadsheetEntry spreadsheet = null;
+
+		// Find spreadsheet by Name
+		for (SpreadsheetEntry spreadsheetEntry : spreadsheets) {
+			if (spreadsheetEntry.getTitle().getPlainText()
+					.equals(spreadsheetName))
+				spreadsheet = spreadsheetEntry;
+		}
+
+		if (spreadsheet == null)
+			throw new IllegalStateException("Spreadsheet Not Found");
+
+		System.out
+				.println("Going to delete all entries from Id: " + email
+						+ " from Spreadsheet: "
+						+ spreadsheet.getTitle().getPlainText());
+
+		// Get the first worksheet of the first spreadsheet.
+		WorksheetFeed worksheetFeed = null;
+		try {
+			worksheetFeed = service.getFeed(spreadsheet.getWorksheetFeedUrl(),
+					WorksheetFeed.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
+		WorksheetEntry worksheet = worksheets.get(0);
+
+		// Fetch the list feed of the worksheet.
+		URL listFeedUrl = worksheet.getListFeedUrl();
+		ListFeed listFeed = null;
+		try {
+			listFeed = service.getFeed(listFeedUrl, ListFeed.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// TODO: Choose a row more intelligently based on your app's needs.
+		ListEntry row = listFeed.getEntries().get(0);
+
+		// Delete the row using the API.
+		try {
+			row.delete();
+		} catch (IOException e) {
+			return false;
+		} catch (ServiceException e) {
+			return false;
+		}
+		return true;
 	}
 
 	private static Form mockActiveSchoolsForm() {
@@ -107,7 +292,8 @@ public class SpreadSheetIntegration {
 		if (spreadsheet == null)
 			throw new IllegalStateException("Spreadsheet Not Found");
 
-		System.out.println(spreadsheet.getTitle().getPlainText());
+		System.out.println("Going to insert Form into: "
+				+ spreadsheet.getTitle().getPlainText());
 
 		// Get the first worksheet of the first spreadsheet.
 		WorksheetFeed worksheetFeed = service.getFeed(
